8dc50d1705c29b0a8df3562dd3c040bddc42576b
joaocastro0429 <joaocastro0429@gmail.com>
2025-11-09 19:04:49 -0300

    atualizando

--- src/main/java/br/com/joaocastro/primeiro_projeto_spring/controllers/PrimeiroController.java
+++ src/main/java/br/com/joaocastro/primeiro_projeto_spring/controllers/PrimeiroController.java
@@ -3,6 +3,8 @@ package br.com.joaocastro.primeiro_projeto_spring.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/primeirocontroller")
@@ -10,6 +12,12 @@ public class PrimeiroController {
     @GetMapping("/primeiro")
     public String primeiroMetodo() {
         return "Olá Mundo! Meu primeiro projeto Spring Boot.";
+    }
+
+    @PostMapping("/create")
+
+    public String Create(@RequestBody String dados){
+        return "Dados recebidos com sucesso: " + dados;
     }
     
 }
